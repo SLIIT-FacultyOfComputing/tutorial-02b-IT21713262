6106a29562141d34d9892197242b43243e919885
IT21713162 <99249707+IT21713262@users.noreply.github.com>
2022-02-14 07:54:25 +0000

    Converted C programs into C++

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,17 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no)
+{
+    int factorial = 1;
+    for (int i = 2; i <= no; i++)
+        factorial = factorial * i;
+    
+    return factorial;
+}
+
+long nCr(int n, int r)
+{
+    return Factorial(n) / (Factorial(r) * Factorial(n - r));
+}
